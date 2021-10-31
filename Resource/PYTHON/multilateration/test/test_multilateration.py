6349ec6981f9843eb62fb97c2969adad7e1a97f4
Hannah C Laugaland <hannahchaplin@hotmail.no>
2021-10-31 19:18:02 +0100

    Temp: Adrian

--- Resource/PYTHON/multilateration/test/test_multilateration.py
+++ Resource/PYTHON/multilateration/test/test_multilateration.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import math
 import multilateration.parameters as param
@@ -42,52 +41,102 @@ import multilateration.multilateration as mult
    * @param bool_valid_parameters An int describing if the parameters
    * given in parameters.h are valid.
 """
-#Test-functions for acoustic trilateration
+# Test-functions for acoustic trilateration
 
 bool_valid_parameters = 0
 
-def valid_number_of_hydrophones(): 
-    return (param.HydrophoneDetailes.NUM_HYDROPHONES < 3)
-        
+
+def valid_number_of_hydrophones():
+    return param.HydrophoneDetailes.NUM_HYDROPHONES < 3
 
 
-
-def calculate_toa_array():  
+def calculate_toa_array():
     """
-    Arg: 
-    in the orginal code is was written: 
+    Arg:
+    in the orginal code is was written:
         uint32_t lag_array[NUM_HYDROPHONES],
         uint8_t& bool_valid_parameters - this one is removed
 
-    return: 
+    return:
     lag_array
-    """  
+    """
 
-    #Calculating the distance [m] between the sound-source and the hydrophones 
-    dist_src_port = float(math.sqrt(math.pow(param.TestParameters.SOURCE_POS_X - param.HydrophoneDetails.PORT_HYD_X, 2) + 
-                                      math.pow(param.TestParameters.SOURCE_POS_Y - param.HydrophoneDetails.PORT_HYD_Y, 2) +
-                                      math.pow(param.TestParameters.SOURCE_POS_Z - param.HydrophoneDetails.PORT_HYD_Z, 2)))
-    dist_src_starboard = float(math.sqrt(math.pow(param.TestParameters.SOURCE_POS_X - param.HydrophoneDetails.STARBOARD_HYD_X, 2) + 
-                                      math.pow(param.TestParameters.SOURCE_POS_Y - param.HydrophoneDetails.STARBOARD_HYD_Y, 2) +
-                                      math.pow(param.TestParameters.SOURCE_POS_Z - param.HydrophoneDetails.STARBOARD_HYD_Z, 2)))
-    dist_src_stern = float(math.sqrt(math.pow(param.TestParameters.SOURCE_POS_X - param.HydrophoneDetails.STERN_HYD_X, 2) + 
-                                      math.pow(param.TestParameters.SOURCE_POS_Y - param.HydrophoneDetails.STERN_HYD_Y, 2) +
-                                      math.pow(param.TestParameters.SOURCE_POS_Z - param.HydrophoneDetails.STERN_HYD_Z, 2)))
+    # Calculating the distance [m] between the sound-source and the hydrophones
+    dist_src_port = float(
+        math.sqrt(
+            math.pow(
+                param.TestParameters.SOURCE_POS_X - param.HydrophoneDetails.PORT_HYD_X,
+                2,
+            )
+            + math.pow(
+                param.TestParameters.SOURCE_POS_Y - param.HydrophoneDetails.PORT_HYD_Y,
+                2,
+            )
+            + math.pow(
+                param.TestParameters.SOURCE_POS_Z - param.HydrophoneDetails.PORT_HYD_Z,
+                2,
+            )
+        )
+    )
+    dist_src_starboard = float(
+        math.sqrt(
+            math.pow(
+                param.TestParameters.SOURCE_POS_X
+                - param.HydrophoneDetails.STARBOARD_HYD_X,
+                2,
+            )
+            + math.pow(
+                param.TestParameters.SOURCE_POS_Y
+                - param.HydrophoneDetails.STARBOARD_HYD_Y,
+                2,
+            )
+            + math.pow(
+                param.TestParameters.SOURCE_POS_Z
+                - param.HydrophoneDetails.STARBOARD_HYD_Z,
+                2,
+            )
+        )
+    )
+    dist_src_stern = float(
+        math.sqrt(
+            math.pow(
+                param.TestParameters.SOURCE_POS_X - param.HydrophoneDetails.STERN_HYD_X,
+                2,
+            )
+            + math.pow(
+                param.TestParameters.SOURCE_POS_Y - param.HydrophoneDetails.STERN_HYD_Y,
+                2,
+            )
+            + math.pow(
+                param.TestParameters.SOURCE_POS_Z - param.HydrophoneDetails.STERN_HYD_Z,
+                2,
+            )
+        )
+    )
 
+    # Calculating the numer of samples it takes for the sound to arrive at x hyd
 
-    #Calculating the numer of samples it takes for the sound to arrive at x hyd
-    
-    #Since the TOA uses lag (direct measuremend), these values are uint32_t 
-    #converting to int32 works as floor. Should be rounded.
-    samp_port = np.int32(param.DSPConstants.SAMPLE_FREQUENCY *(dist_src_port / param.PhysicalConstants.SOUND_SPEED)) # 
-    samp_starboard = np.int32(param.DSPConstants.SAMPLE_FREQUENCY *(dist_src_starboard / param.PhysicalConstants.SOUND_SPEED)) # 
-    samp_stern = np.int32(param.DSPConstants.SAMPLE_FREQUENCY*(dist_src_stern / param.PhysicalConstants.SOUND_SPEED)) #* 
+    # Since the TOA uses lag (direct measuremend), these values are uint32_t
+    # converting to int32 works as floor. Should be rounded.
+    samp_port = np.int32(
+        param.DSPConstants.SAMPLE_FREQUENCY
+        * (dist_src_port / param.PhysicalConstants.SOUND_SPEED)
+    )  #
+    samp_starboard = np.int32(
+        param.DSPConstants.SAMPLE_FREQUENCY
+        * (dist_src_starboard / param.PhysicalConstants.SOUND_SPEED)
+    )  #
+    samp_stern = np.int32(
+        param.DSPConstants.SAMPLE_FREQUENCY
+        * (dist_src_stern / param.PhysicalConstants.SOUND_SPEED)
+    )  # *
 
     lag_port_starboard = samp_port - samp_starboard
     lag_port_stern = samp_port - samp_stern
     lag_startboard_stern = samp_starboard - samp_stern
 
     return [lag_port_starboard, lag_port_stern, lag_startboard_stern]
+
 
 """
    * @brief Function that checks if the simulated sound-source position is  within 
@@ -97,35 +146,39 @@ def calculate_toa_array():
 """
 
 
-
 def test_trilateration_algorithm():
 
     """
-    Creating the TOA-intervals. 
+    Creating the TOA-intervals.
     Writing out an error-msg if the parameters are wrong
     """
 
     lag_array = calculate_toa_array()
-    
-    if(not valid_number_of_hydrophones):
+
+    if not valid_number_of_hydrophones:
         print("\nAt least one parameter is invalid")
         return
 
-   
+    # Initializing the estimate for x-pos and y-pos
 
-    #Initializing the estimate for x-pos and y-pos
-
-    x_pos_es, y_pos_es = mult.trilaterate_pinger_position(lag_array) 
+    x_pos_es, y_pos_es = mult.trilaterate_pinger_position(lag_array)
 
     """
     * Calculating the distance between the estimated position and the 
     * actual position. Only considering x and y, since we only have
     * three hydrophones
     """
-    distance_diff = float(math.sqrt(
-            math.pow(x_pos_es - param.TestParameters.SOURCE_POS_X, 2) +
-            math.pow(y_pos_es - param.TestParameters.SOURCE_POS_Y, 2)))
+    distance_diff = float(
+        math.sqrt(
+            math.pow(x_pos_es - param.TestParameters.SOURCE_POS_X, 2)
+            + math.pow(y_pos_es - param.TestParameters.SOURCE_POS_Y, 2)
+        )
+    )
 
     print(f"\nThe estimated pinger position is at (x,y) = ({x_pos_es}, {y_pos_es})")
-    print(f"\nThe actual pinger position is at (x,y) = ({param.TestParameters.SOURCE_POS_X}, {param.TestParameters.SOURCE_POS_Y)})")
-    print(f"\nThe difference between the actual position and the estimated position is {distance_diff} m")
+    print(
+        f"\nThe actual pinger position is at (x,y) = ({param.TestParameters.SOURCE_POS_X}, {param.TestParameters.SOURCE_POS_Y})"
+    )
+    print(
+        f"\nThe difference between the actual position and the estimated position is {distance_diff} m"
+    )
